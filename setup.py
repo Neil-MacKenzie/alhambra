f9e78eeedf59c44ac0e85bd6dfaa9300b302e784
Constantine Evans <cge@dna.caltech.edu>
2015-09-29 15:55:08 -0700

    Various changes to diagrams, removed spuriousc.py, bump to 0.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "tilesetdesigner",
-    version = "0.0.1dev",
+    version = "0.0.1",
     packages = ['tilesetdesigner']
 
     install_requires = ['numpy','stickydesign','svgwrite','lxml'],
