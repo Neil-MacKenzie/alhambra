938fb4229143f50a8d2c2d85bc37c0e82822025b
Constantine Evans <cevans@evanslabs.org>
2017-08-24 21:55:03 -0700

    v0.4.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "alhambra",
-    version = "0.4.0",
+    version = "0.4.1",
     packages = ['alhambra'],
 
     install_requires = ['numpy','stickydesign >= 0.5.0','svgwrite','lxml','shutilwhich','peppercompiler', 'ruamel.yaml'],
