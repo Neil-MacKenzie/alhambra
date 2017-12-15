b764b6cae5ef867cf3fbb1076da98de66152dc23
Constantine Evans <cevans@evanslabs.org>
2017-12-15 11:54:43 -0800

    Update install_requires, format setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,15 @@
 from setuptools import setup
 
 setup(
-    name = "alhambra",
-    version = "1.0.0.dev1",
-    packages = ['alhambra'],
-
-    install_requires = ['numpy','stickydesign >= 0.5.0','svgwrite','lxml','shutilwhich','peppercompiler', 'ruamel.yaml'],
-
+    name="alhambra",
+    version="1.0.0.dev1",
+    packages=['alhambra'],
+    install_requires=[
+        'numpy', 'stickydesign >= 0.6.0', 'svgwrite', 'lxml', 'shutilwhich',
+        'peppercompiler', 'ruamel.yaml', 'cssutils'
+    ],
     include_package_data=True,
-
-    entry_points={
-        'console_scripts': [
-            'alhambra = alhambra.scripts:alhambra'
-            ]
-        },
-    author = "Constantine Evans",
-    author_email = "cgevans@evans.foundation",
-    description = "DX Tile Set Designer",
-)
+    entry_points={'console_scripts': ['alhambra = alhambra.scripts:alhambra']},
+    author="Constantine Evans",
+    author_email="cgevans@evans.foundation",
+    description="DX Tile Set Designer", )
