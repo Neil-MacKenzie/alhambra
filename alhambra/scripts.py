9ac3104f0be3aa681cd988c48297bc1ab5b6e317
Constantine Evans <cevans@evanslabs.org>
2018-03-12 19:45:15 -0700

    start new script interface

--- alhambra/scripts.py
+++ alhambra/scripts.py
@@ -3,95 +3,80 @@ import argparse
 import logging
 import sys
 import os.path
+from .tilesets import TileSet
+import logging
+import click
 
-def alhambra():
-    import sys
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("inputfile", help="tileset input YAML file")
-    parser.add_argument("-n", "--name", help="base name for output files")
-    parser.add_argument("-o", "--out", help="output file (if not just NAME-out.yaml)")
-    parser.add_argument("-r", "--reorderargs", help="extra arguments (as dict) to reorder function", default="{}")
-    parser.add_argument("-s", "--spuriousargs", help="extra params (use \"s) for spuriousSSM", default="")
-    parser.add_argument("-v", "--verbose", help="increase verbosity", action="store_true", default=False)
-    parser.add_argument("-f", "--force", help="overwrite output file", default=False, action="store_true")
-    parser.add_argument("-d", "--diagrams", help="make diagrams as well", default=False, action="store_true")
-    parser.add_argument("-x", "--xgrow", help="make xgrow files as well (obsolete, use xgrow_wrap)", default=False, action="store_true")
-    parser.add_argument("-T", help="temperature for energetics model", default=37)
-    parser.add_argument("-m", "--mismatch", help="mismatch model (max,dangle,loop,combined)", default="max")
-    parser.add_argument("-i", "--include", help="add pepper include path", default=None, action="append")
-    parser.add_argument("--singlepair", help="enable single base pairs for combined energetics model", default=False,
-            action="store_true")
-    parser.add_argument("--test", help="run quickly for testing, making bad sequences", default=False, action="store_true")
-    args = parser.parse_args()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-    
-    if args.verbose:
-        logging.getLogger().setLevel(logging.DEBUG)
+POOL = None
+
+@click.group(context_settings=CONTEXT_SETTINGS, chain=True)
+@click.argument('filename', type=click.Path(exists=True))
+@click.option('--verbose/--quiet', '-v/-q', default=False)
+@click.option(
+    '--output',
+    '-o',
+    type=click.Path(exists=False),
+    default=None,
+    help='output file name (defaults to stdout)')
+def alhambra(filename, verbose, output):
+    pass
+
+
+@alhambra.resultcallback()
+def process_pipeline(processors, filename, verbose, output):
+    if verbose:
+        logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    ts = TileSet.from_file(filename)
+    for processor in processors:
+        ts = processor(ts)
+    if output is None:
+        ts.to_file(sys.stdout)
     else:
-        logging.getLogger().setLevel(logging.INFO)
-    
-    base = os.path.splitext(os.path.basename(args.inputfile))[0]
-    
-    if not args.name:
-        args.name = base
-    else:
-        base = args.name
-    if not args.out:
-        args.out = base+'-out.yaml'
+        ts.to_file(output)
 
-    args.reorderargs=eval(args.reorderargs) # FIXME: highly dangerous
-    if args.test:
-        args.reorderargs['steps']=5
-        args.spuriousargs+=" bored=5"
 
-    # Check that we're not clobbering something here for the output. FIXME: do
-    # this for temp files too.
-    if os.path.exists(args.out) and not args.force:
-        logging.error("Output file already exists, and --force is not enabled.")
-        sys.exit(1)
+@alhambra.command(short_help='generate sticky end sequences')
+@click.option('--method', '-m', type=click.Choice(['default', 'multimodel']), help='stickydesign method to use (multimodel or default)', default='default')
+@click.option('--trials', '-n', default=100, help='number of trials (defaults to 100)')
+def create_end_sequences(method, trials):
+    """generates sticky end sequences for the system."""
+    def processor(ts):
+        ts_new, _ = ts.create_end_sequences(method=method, trials=trials)
+        return ts_new
+    return processor
 
-    for ext in ['.sys','.save','.mfe','.fix','.pil','.seqs']:
-        if os.path.exists(args.name+ext) and not args.force:
-            logging.error("Temporary file {} already exists, and --force is not enabled.".format(args.name+ext))
-            sys.exit(1)
 
-    if not os.path.exists(args.inputfile):
-        logging.error("Input file does not exist.")
-        sys.exit(1)
+@alhambra.command(short_help='attempt to reduce the # of sticky ends')
+@click.option('--latticedefects/--no-latticedefects', '-l/-L', default=True)
+@click.option('--sclass', '-s', multiple=True, default=['2GO'])
+@click.option('--trials', '-n', default=1)
+def reduce_ends(latticedefects, sclass, trials):
+    def processor(ts):
+        ts_new = ts.reduce_ends(checkld=latticedefects, _classes=tuple(sclass))
+        return ts_new
+    return processor
 
-    from . import designer
+@alhambra.command(short_help='attempt to reduce the # of tiles')
+@click.option('--latticedefects/--no-latticedefects', '-l/-L', default=True)
+@click.option('--sclass', '-s', multiple=True, default=['2GO'])
+@click.option('--rotate/--no-rotate', '-r/-R', default=True)
+def reduce_tiles(latticedefects, sclass, rotate):
+    def processor(ts):
+        ts_new = ts.reduce_tiles(checkld=latticedefects, _classes=tuple(sclass),
+                                 rotation=rotate)
+        return ts_new
+    return processor
 
-    from stickydesign.energetics_daoe import energetics_daoe
-    energetics = energetics_daoe(temperature=float(args.T),singlepair=args.singlepair,mismatchtype=args.mismatch)
+@alhambra.command(short_help='optimize sticky end assignment')
+def reorder_ends():
+    pass
 
-    otherargs = {}
-    if args.include:
-        otherargs['includes'] = args.include
+@alhambra.command(short_help='create strand sequences')
+def create_strand_sequences():
+    pass
 
-    sys = designer.design_set( \
-            args.inputfile, 
-            args.name,
-            reorderopts=args.reorderargs,
-            coreopts={'spurious_pars': args.spuriousargs},
-            stickyopts={'energetics': energetics},
-            **otherargs)
-
-    yaml.dump(sys, open(args.out,'w'))
-
-    if args.diagrams:
-        designer.create_abstract_diagrams( sys , base+'-abstract.svg' )
-        designer.create_sequence_diagrams( sys, base+'-sequences.svg' )
-        if 'createseqs' in sys['seed'].keys():
-            designer.create_adapter_sequence_diagrams( sys, base+'-adapters.svg' )
-
-    if args.xgrow:
-        from . import stxg
-        yaml.dump( stxg.from_yaml_endadj( sys, perfect=True ), 
-                   open(base+'-perfectends.stxg', 'w') )
-        yaml.dump( stxg.from_yaml_endadj( sys, perfect=True, rotate=True ), 
-                   open(base+'-perfectends-rotated.stxg', 'w') )
-        yaml.dump( stxg.from_yaml_endadj( sys, perfect=False ), 
-                   open(base+'-withends.stxg', 'w') )
-        yaml.dump( stxg.from_yaml_endadj( sys, perfect=False, rotate=True ), 
-                   open(base+'-withends-rotated.stxg', 'w') )
+@alhambra.command(short_help='run xgrow')
+def run_xgrow():
+    pass
