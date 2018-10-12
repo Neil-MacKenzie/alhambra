de6f150cf6a028313d66e47e4a487dfa600fb427
Constantine Glen Evans <cevans@evanslabs.org>
2018-10-12 17:16:33 +0800

    try to fix some issues

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="1.0.0.dev1",
     packages=['alhambra'],
     install_requires=[
-        'numpy', 'stickydesign >= 0.6.0', 'svgwrite', 'lxml', 'shutilwhich',
+        'numpy', 'stickydesign >= 0.7.1', 'svgwrite', 'lxml', 'shutilwhich',
         'peppercompiler >= 0.1.2', 'ruamel.yaml', 'cssutils'
     ],
     include_package_data=True,
