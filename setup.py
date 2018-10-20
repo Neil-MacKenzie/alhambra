84409db461011859dd93013011ea50804dd210da
Constantine Glen Evans <cevans@evanslabs.org>
2018-10-19 18:30:35 -0700

    Bump versions

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(
     name="alhambra",
-    version="1.0.0.dev1",
+    version="1.0.0.dev2",
     packages=['alhambra'],
     install_requires=[
-        'numpy', 'stickydesign >= 0.7.1', 'svgwrite', 'lxml', 'shutilwhich',
+        'numpy', 'stickydesign >= 0.8.0', 'svgwrite', 'lxml', 'shutilwhich',
         'peppercompiler >= 0.1.2', 'ruamel.yaml', 'cssutils'
     ],
     include_package_data=True,
