216881a74b0cf35209d493ffcff6a4a168a0a2d1
Constantine Glen Evans <cevans@evanslabs.org>
2018-10-19 18:44:34 -0700

    another stickydesign bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="1.0.0.dev2",
     packages=['alhambra'],
     install_requires=[
-        'numpy', 'stickydesign >= 0.8.0', 'svgwrite', 'lxml', 'shutilwhich',
+        'numpy', 'stickydesign >= 0.8.1', 'svgwrite', 'lxml', 'shutilwhich',
         'peppercompiler >= 0.1.2', 'ruamel.yaml', 'cssutils'
     ],
     include_package_data=True,
